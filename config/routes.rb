f286ef4a3ca519d86a19a8c365d4d14f916d64ab
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-05 18:28:27 -0600

    Added an idea scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ideator::Application.routes.draw do
+  resources :ideas
+
+  # root to: 'ideas#index'
+
+  get '/' => 'ideas#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
